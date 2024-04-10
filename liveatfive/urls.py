b9c194b99f266cd6aedbcf9d4bf0a50c692e4b10
lusciousdev <admin@luscious.dev>
2024-04-10 01:10:28 -0500

    add trailing slashes

--- liveatfive/urls.py
+++ liveatfive/urls.py
@@ -6,11 +6,11 @@ from . import api
 app_name = 'liveatfive'
 urlpatterns = [
   path("", views.IndexView.as_view(), name="index"),
-  path("faq", views.FAQView.as_view(), name="faq"),
-  path("stats", views.StatsView.as_view(), name="stats"),
-  path("api/v1/record", api.get_record, name="api_record"),
-  path("api/v1/live", api.get_is_live, name="api_live"),
-  path("api/v1/history", api.get_history, name="api_history"),
-  path("api/v1/when", api.get_when_live, name="api_when"),
-  path("api/v1/streaks", api.get_streaks, name="api_streaks"),
+  path("faq/", views.FAQView.as_view(), name="faq"),
+  path("stats/", views.StatsView.as_view(), name="stats"),
+  path("api/v1/record/", api.get_record, name="api_record"),
+  path("api/v1/live/", api.get_is_live, name="api_live"),
+  path("api/v1/history/", api.get_history, name="api_history"),
+  path("api/v1/when/", api.get_when_live, name="api_when"),
+  path("api/v1/streaks/", api.get_streaks, name="api_streaks"),
 ]
